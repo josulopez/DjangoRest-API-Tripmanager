4e404feb051b5bffa7149f189c59ce080845a7b4
Josué López <josue.lopez.21005@gmail.com>
2025-09-22 11:59:27 -0600

    Modelo Viaje

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+ class Viaje(models.Model):
+     destino = models.CharField(max_length=100)
+     fecha_salida = models.DateField()
+     fecha_regreso = models.DateField()
+     tipo_viaje = models.BooleanField(default=False)  # False para Vacaciones, True para Negociosl
+
+     def __str__(self):
+         return f"{self.destino} {self.fecha_salida} al {self.fecha_regreso}"
